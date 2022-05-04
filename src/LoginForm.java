a94fda8dcf821591a200822c398ca4fa7f43977f
Krigovszki Bálint <krigovszki.balint@gmail.com>
2022-05-04 15:01:05 +0200

    Bejelentkezési felület létrehozása

---
+++ src/LoginForm.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class LoginForm {
+    private JPanel loginPanel;
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+    private JButton bejelentkezésButton;
+    private JButton kilépésButton;
+}
