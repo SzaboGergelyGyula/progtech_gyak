5f26a9b70f51389bcf883bac6b18e8c153af1be7
Krigovszki Bálint <krigovszki.balint@gmail.com>
2022-05-04 15:21:15 +0200

    Bejelentkezés implementálása

--- src/LoginForm.java
+++ src/LoginForm.java
@@ -2,8 +2,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.StandardSocketOptions;
+import java.sql.*;
 
-public class LoginForm extends JDialog{
+public class LoginForm extends JDialog {
     private JPanel loginPanel;
     private JTextField tfEmail;
     private JPasswordField pfPassword;
@@ -14,26 +16,84 @@ public class LoginForm extends JDialog{
         super(parent);
         setTitle("Bejelentkezés");
         setContentPane(loginPanel);
-        setMinimumSize(new Dimension(550, 450));
+        setMinimumSize(new Dimension(550,450));
         setModal(true);
         setLocationRelativeTo(parent);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        setVisible(true);
+
         btnLogin.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                String email = tfEmail.getText();
+                String password = String.valueOf(pfPassword.getPassword());
 
+                user = getAuthenticatedUser(email, password);
+
+                if (user != null)
+                {
+                    dispose();
+                }
+                else {
+                    JOptionPane.showMessageDialog(LoginForm.this,
+                            "E-mail cím vagy jelszó helytelen!",
+                            "Sikertelen bejelentkezés",
+                            JOptionPane.ERROR_MESSAGE);
+                }
             }
         });
         btnClose.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                dispose();
             }
         });
+
+        setVisible(true);
+    }
+    public User user;
+    private User getAuthenticatedUser(String email, String password) {
+        User user = null;
+
+        final String DB_URL = "jdbc:mysql://localhost/progtech_kdpeq8_c54c3q";
+        final String USERNAME = "root";
+        final String PASSWORD = "";
+
+        try {
+            Connection conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+
+            Statement stmt = conn.createStatement();
+            String sql = "SELECT * FROM users WHERE email=? AND password=?";
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setString(1, email);
+            preparedStatement.setString(2, password);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                user = new User();
+                user.username = resultSet.getString("username");
+                user.email = resultSet.getString("email");
+                user.password = resultSet.getString("password");
+            }
+
+            stmt.close();
+            conn.close();
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        return user;
     }
 
     public static void main(String[] args) {
         LoginForm loginForm = new LoginForm(null);
+        User user = loginForm.user;
+        if (user != null) {
+            System.out.println("Sikeres bejelentkezés: "+user.username);
+            System.out.println("E-mail: "+user.email);
+        } else {
+            System.out.println("Bejelentkezés megszakítva");
+        }
     }
 }
