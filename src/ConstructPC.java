b12f4868bdf6693fc59f8095acf5988916531b05
Krigovszki Bálint <krigovszki.balint@gmail.com>
2022-05-05 05:21:14 +0200

    Számítógép Összeállító GUI

--- src/ConstructPC.java
+++ src/ConstructPC.java
@@ -7,13 +7,21 @@ public class ConstructPC extends JDialog{
 
     private JPanel constructPanel;
     private JButton btnLogout;
+    private JList listComputersBuilt;
+    private JComboBox cbComputerType;
+    private JComboBox cbCPU;
+    private JTextField tfRAM;
+    private JTextField tfStorage;
+    private JComboBox cbGPU;
+    private JLabel lblLastPC;
+    private JButton btnCreatePC;
 
     // ConstructPC konstruktor
     public ConstructPC(JFrame parent) {
         super(parent);
         setTitle("Számítógép összeállítása");
         setContentPane(constructPanel);
-        setMinimumSize(new Dimension(900, 600));
+        setMinimumSize(new Dimension(1000, 750));
         setModal(true);
         setLocationRelativeTo(parent);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
