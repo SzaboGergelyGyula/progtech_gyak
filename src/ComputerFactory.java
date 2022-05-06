bf4a4eb8ee9b3ff82ae4535f77337eff36645b9a
Krigovszki Bálint <krigovszki.balint@gmail.com>
2022-05-06 03:33:27 +0200

    Computer Factory implementálása

--- src/ComputerFactory.java
+++ src/ComputerFactory.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class ComputerFactory {
+public class ComputerFactory {
+    public static Computer getComputer(String computerType){
+        if(computerType == null){
+            return null;
+        }
+        if(computerType.equalsIgnoreCase("PC")){
+            return new PC();
+
+        } else if(computerType.equalsIgnoreCase("Server")){
+            return new Server();
+
+        } else if(computerType.equalsIgnoreCase("Notebook")){
+            return new Notebook();
+        }
+
+        return null;
+    }
 }
